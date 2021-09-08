39a4cb17e79da8e58a781f3b8a68ecc8bddd7e68
Mario Migliacio <v-mamigl@microsoft.com>
2021-09-08 12:39:05 -0700

    saving point before event bubbling

--- Hooks/src/components/Dropdown.js
+++ Hooks/src/components/Dropdown.js
@@ -1,30 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
-    const renderedOptions = options.map((option) => {
-      return (
-        <div
-          className="item"
-          key={option.value}
-          onClick={() => onSelectedChange(option)}
-        >
-          {option.label}
-        </div>
-      );
-    });
+  const [open, setOpen] = useState(false);
+
+  const renderedOptions = options.map((option) => {
+    if (option.value === selected.value) {
+      return null;
+    }
 
     return (
-      <div className="ui form">
-        <div className="field">
-          <label className="label">Select a Color</label>
-          <div className="ui selection dropdown visible active">
-            <i className="dropdown icon"></i>
-            <div className="text">{selected.label}</div>
-            <div className="menu visible transition">{renderedOptions}</div>
-          </div>
-        </div>
+      <div
+        className="item"
+        key={option.value}
+        onClick={() => onSelectedChange(option)}
+      >
+        {option.label}
       </div>
     );
-}
+  });
+
+  return (
+    <div className="ui form">
+      <div className="field">
+        <label className="label">Select a Color</label>
+        <div onClick={() => setOpen(!open)} className="ui selection dropdown">
+          <i className="dropdown icon"></i>
+          <div className="text">{selected.label}</div>
+          <div className="menu">{renderedOptions}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Dropdown;
