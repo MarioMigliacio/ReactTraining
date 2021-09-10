c1244981f01247c5b9627bfbefd43fd15fede25a
Mario Migliacio <v-mamigl@microsoft.com>
2021-09-10 14:39:18 -0700

    alt solution: adding another action creator

--- blogpost/src/actions/index.js
+++ blogpost/src/actions/index.js
@@ -1,19 +1,29 @@
 import jsonPlaceHolder from "../apis/jsonPlaceHolder";
 import _ from 'lodash';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach(id => dispatch(fetchUser(id)));
+}
+
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceHolder.get("/posts");
 
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = (id) => (dispatch) => {
-    _fetchUser(id, dispatch);
-};
-
-// private memoized function. Purpose: don't call network requests on previously already requested.
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async (dispatch) => {
   const response = await jsonPlaceHolder.get(`/users/${id}`);
 
   dispatch({ type: "FETCH_USER", payload: response.data });
-});
+};
+
+
+// private memoized function. Purpose: don't call network requests on previously already requested.
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceHolder.get(`/users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
