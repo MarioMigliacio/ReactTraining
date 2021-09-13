b4e9c4eb80aa30d34c31e869d83a3c4298656fa0
Mario Migliacio <v-mamigl@microsoft.com>
2021-09-13 15:34:27 -0700

    google auth button working

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -23,10 +23,32 @@ class GoogleAuth extends Component {
       this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   }
 
+  onSignIn = () => {
+    this.auth.signIn();
+  }
+
+  onSignOut = () => {
+    this.auth.signOut();
+  }
+
   RenderAuthButton() {
-      if (this.state.isSignedIn === null || this.state.isSignedIn === false) {
-          return <div>Sign In</div>
-      } else return <div>Sign Out</div> 
+    if (this.state.isSignedIn === null) {
+      return null;
+    } else if (this.state.isSignedIn) {
+      return (
+        <button className="ui red google button" onClick={this.onSignOut}>
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
+    } else {
+      return (
+        <button className="ui red google button" onClick={this.onSignIn}>
+          <i className="google icon" />
+          Sign In
+        </button>
+      );
+    }
   }
 
   render() {
