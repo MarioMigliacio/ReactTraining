5006fd9587a0d1970b08cd561ee720af2dac0eea
Mario Migliacio <v-mamigl@microsoft.com>
2021-09-15 14:56:44 -0700

    scaffolding

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,13 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import UserCreate from "./UserCreate";
 
-export class App extends Component {
-    render() {
-        return (
-            <div>
-                App
-            </div>
-        )
-    }
+class App extends Component {
+  state = { language: "english" };
+
+  onLanguageChange = (language) => {
+    this.setState({language});
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a language: <br />
+          <i className="flag us" onClick={() => this.onLanguageChange("english")} />
+          <i className="flag nl" onClick={() => this.onLanguageChange("dutch")} />
+        </div>
+
+        <UserCreate />
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
